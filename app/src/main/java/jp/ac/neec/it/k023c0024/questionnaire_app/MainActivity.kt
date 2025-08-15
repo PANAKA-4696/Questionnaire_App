7b04d5e24a54c85fdd189cac800a20be43240122
PANAKA-4696 <212013796+PANAKA-4696@users.noreply.github.com>
2025-08-15 11:48:19 +0900

    ボタンを実行可能に反映

--- app/src/main/java/jp/ac/neec/it/k023c0024/questionnaire_app/MainActivity.kt
+++ app/src/main/java/jp/ac/neec/it/k023c0024/questionnaire_app/MainActivity.kt
@@ -1,6 +1,9 @@
 package jp.ac.neec.it.k023c0024.questionnaire_app
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -10,5 +13,25 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val bt_new = findViewById<Button>(R.id.btNew)
+        val bt_member = findViewById<Button>(R.id.btMember)
+
+        bt_new.setOnClickListener(ClickNewCustomerListener())
+        bt_member.setOnClickListener(ClickAddCustomerInformationListener())
+    }
+
+    private inner class ClickNewCustomerListener : View.OnClickListener {
+        override fun onClick(v: View?) {
+            val NewCustomer = Intent(this@MainActivity, NewCustomer::class.java)
+            startActivity(NewCustomer)
+        }
+    }
+
+    private inner class ClickAddCustomerInformationListener : View.OnClickListener {
+        override fun onClick(v: View?) {
+            val AddCustomerInformation = Intent(this@MainActivity, AddCustomerInformation::class.java)
+            startActivity(AddCustomerInformation)
+        }
     }
 }

ba3893ff294f912c3f7e01226858ba43046df90e
PANAKA-4696 <212013796+PANAKA-4696@users.noreply.github.com>
2025-08-16 22:13:39 +0900

    バージョンの更新

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "jp.ac.neec.it.k023c0024.questionnaire_app"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "jp.ac.neec.it.k023c0024.questionnaire_app"

d41c7a3b4c2d54b439e0032b8d06ce2455f463ec
PANAKA-4696 <212013796+PANAKA-4696@users.noreply.github.com>
2025-08-17 12:01:39 +0900

    新規登録の確認画面の追加

---
+++ app/src/main/java/jp/ac/neec/it/k023c0024/questionnaire_app/NewCustomerConfirm.kt
@@ -0,0 +1,14 @@
+package jp.ac.neec.it.k023c0024.questionnaire_app
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class NewCustomerConfirm : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_new_customer_confirm)
+    }
+}

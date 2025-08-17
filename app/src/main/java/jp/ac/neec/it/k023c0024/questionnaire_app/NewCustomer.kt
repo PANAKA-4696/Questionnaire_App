641a410838baf3893530062edabe2f75e9e2725c
PANAKA-4696 <212013796+PANAKA-4696@users.noreply.github.com>
2025-08-17 12:07:27 +0900

    新規登録の確認画面に遷移するためのボタンの実行化

--- app/src/main/java/jp/ac/neec/it/k023c0024/questionnaire_app/NewCustomer.kt
+++ app/src/main/java/jp/ac/neec/it/k023c0024/questionnaire_app/NewCustomer.kt
@@ -1,6 +1,9 @@
 package jp.ac.neec.it.k023c0024.questionnaire_app
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -10,5 +13,14 @@ class NewCustomer : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_new_customer)
+
+        val bt_put_information = findViewById<Button>(R.id.btPutInfomation)
+    }
+
+    private inner class ClickPutInformationListener : View.OnClickListener {
+        override fun onClick(v: View?) {
+            val PutInformation = Intent(this@NewCustomer, NewCustomerConfirm::class.java)
+            startActivity(PutInformation)
+        }
     }
 }

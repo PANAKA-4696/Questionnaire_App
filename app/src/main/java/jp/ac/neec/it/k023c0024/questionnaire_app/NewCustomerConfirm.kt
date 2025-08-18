5d57043694c13179e5bd327e76dfd0f92c0a191f
PANAKA-4696 <212013796+PANAKA-4696@users.noreply.github.com>
2025-08-18 14:43:44 +0900

    確認画面への転送確認

--- app/src/main/java/jp/ac/neec/it/k023c0024/questionnaire_app/NewCustomerConfirm.kt
+++ app/src/main/java/jp/ac/neec/it/k023c0024/questionnaire_app/NewCustomerConfirm.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.MenuItem
 import android.view.View
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -20,6 +21,50 @@ class NewCustomerConfirm : AppCompatActivity() {
 
         bt_save.setOnClickListener(ClickSaveListener())
         bt_cancel.setOnClickListener(ClickCancelListener())
+
+        val kana = intent.getStringExtra("kana")
+        val name = intent.getStringExtra("name")
+        val sex = intent.getStringExtra("sex")
+        val era = intent.getStringExtra("era")
+        val year = intent.getStringExtra("year")
+        val month = intent.getStringExtra("month")
+        val day = intent.getStringExtra("day")
+        val old = intent.getStringExtra("old")
+        val zip1 = intent.getStringExtra("zip1")
+        val zip2 = intent.getStringExtra("zip2")
+        val tel = intent.getStringExtra("tel")
+        val address = intent.getStringExtra("address")
+        val mail = intent.getStringExtra("mail")
+        val role = intent.getStringExtra("role")
+
+        val puKana =findViewById<TextView>(R.id.tvNewCustomerConfirmKana)
+        puKana.text = kana
+        val puName =findViewById<TextView>(R.id.tvNewCustomerConfirmName)
+        puName.text = name
+        val puSex =findViewById<TextView>(R.id.tvNewCustomerConfirmSex)
+        puSex.text = sex
+        val puEra =findViewById<TextView>(R.id.tvNewCustomerConfirmEra)
+        puEra.text = era
+        val puYear =findViewById<TextView>(R.id.tvNewCustomerConfirmYear)
+        puYear.text = year
+        val puMonth =findViewById<TextView>(R.id.tvNewCustomerConfirmMonth)
+        puMonth.text = month
+        val puDay =findViewById<TextView>(R.id.tvNewCustomerConfirmDay)
+        puDay.text = day
+        val puOld =findViewById<TextView>(R.id.tvNewCustomerConfirmOld)
+        puOld.text = old
+        val puZip1 =findViewById<TextView>(R.id.tvNewCustomerConfirmZip1)
+        puZip1.text = zip1
+        val puZip2 =findViewById<TextView>(R.id.tvNewCustomerConfirmZip2)
+        puZip2.text = zip2
+        val puTel =findViewById<TextView>(R.id.tvNewCustomerConfirmTel)
+        puTel.text = tel
+        val puAddress =findViewById<TextView>(R.id.tvNewCustomerConfirmAddress)
+        puAddress.text = address
+        val puMail =findViewById<TextView>(R.id.tvNewCustomerConfirmMail)
+        puMail.text = mail
+        val puRole =findViewById<TextView>(R.id.tvNewCustomerConfirmRole)
+        puRole.text = role
 
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
     }

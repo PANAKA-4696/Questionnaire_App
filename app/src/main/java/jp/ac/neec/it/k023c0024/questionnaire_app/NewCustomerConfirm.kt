15fdb7e3b25ba8d174b1db716001d11275e1bb06
PANAKA-4696 <212013796+PANAKA-4696@users.noreply.github.com>
2025-08-18 13:39:29 +0900

    キャンセルボタンの有効化

--- app/src/main/java/jp/ac/neec/it/k023c0024/questionnaire_app/NewCustomerConfirm.kt
+++ app/src/main/java/jp/ac/neec/it/k023c0024/questionnaire_app/NewCustomerConfirm.kt
@@ -1,6 +1,9 @@
 package jp.ac.neec.it.k023c0024.questionnaire_app
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -10,5 +13,27 @@ class NewCustomerConfirm : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_new_customer_confirm)
+
+        val bt_save = findViewById<Button>(R.id.btSave)
+        val bt_cancel = findViewById<Button>(R.id.btCancel)
+
+        bt_save.setOnClickListener(ClickSaveListener())
+        bt_cancel.setOnClickListener(ClickCancelListener())
+    }
+
+    private inner class ClickSaveListener : View.OnClickListener {
+        override fun onClick(v: View?) {
+            val msg = "保存しました"
+            Toast.makeText(this@NewCustomerConfirm, msg, Toast.LENGTH_LONG).show()
+        }
+    }
+
+    private inner class ClickCancelListener : View.OnClickListener {
+        override fun onClick(v: View?) {
+            val msg = "キャンセルしました"
+            Toast.makeText(this@NewCustomerConfirm, msg, Toast.LENGTH_LONG).show()
+
+            finish()
+        }
     }
 }

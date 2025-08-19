79f9944f82fa82ac9cc974a2ff4c9979095ddd8b
PANAKA-4696 <212013796+PANAKA-4696@users.noreply.github.com>
2025-08-19 11:51:32 +0900

    ConfirmDialogFragmentのクラスを作成

---
+++ app/src/main/java/jp/ac/neec/it/k023c0024/questionnaire_app/ConfirmDialogFragment.kt
@@ -0,0 +1,9 @@
+package jp.ac.neec.it.k023c0024.questionnaire_app
+
+import android.app.Dialog
+import android.os.Bundle
+import androidx.fragment.app.DialogFragment
+
+class ConfirmDialogFragment: DialogFragment() {
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+}

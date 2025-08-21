f9d83ffe3c987947f479f1cbbac06ffadf0265ce
PANAKA-4696 <212013796+PANAKA-4696@users.noreply.github.com>
2025-08-21 15:03:11 +0900

    ファイル名の変更

---
+++ app/src/main/java/jp/ac/neec/it/k023c0024/questionnaire_app/Questionnaire.kt
@@ -0,0 +1,11 @@
+package jp.ac.neec.it.k023c0024.questionnaire_app
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class Questionnaire : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_questionnaire)
+    }
+}

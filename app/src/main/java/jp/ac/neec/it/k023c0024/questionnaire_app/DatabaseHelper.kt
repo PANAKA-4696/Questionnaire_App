257dfd2dce6c89363205837ef587e65314bf1638
PANAKA-4696 <212013796+PANAKA-4696@users.noreply.github.com>
2025-08-21 14:40:12 +0900

    companion objectでバージョンやDB名の定義

--- app/src/main/java/jp/ac/neec/it/k023c0024/questionnaire_app/DatabaseHelper.kt
+++ app/src/main/java/jp/ac/neec/it/k023c0024/questionnaire_app/DatabaseHelper.kt
@@ -7,6 +7,10 @@ import android.database.sqlite.SQLiteOpenHelper
 class DatabaseHelper (context: Context): SQLiteOpenHelper(context, DATABASE_NAME, null, DATABASE_VERSION){
     //クラス内のprivate定数を宣言するためにcompanion objectブロックとする。
     companion object{
+        //データベースファイル名の定数。
+        private const val DATABASE_NAME = "customer.db"
+        //バージョン情報の定数
+        private const val DATABASE_VERSION = 1
     }
 
     override fun onCreate(db: SQLiteDatabase){

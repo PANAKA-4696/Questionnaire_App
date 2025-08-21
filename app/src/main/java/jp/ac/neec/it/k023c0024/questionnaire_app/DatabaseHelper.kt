04411f6d64f53ec22ad8e16c94d3b1159fe15a96
PANAKA-4696 <212013796+PANAKA-4696@users.noreply.github.com>
2025-08-21 14:33:58 +0900

    DatabaseHelperのメイン構成処理の大枠の作成

--- app/src/main/java/jp/ac/neec/it/k023c0024/questionnaire_app/DatabaseHelper.kt
+++ app/src/main/java/jp/ac/neec/it/k023c0024/questionnaire_app/DatabaseHelper.kt
@@ -1,4 +1,16 @@
 package jp.ac.neec.it.k023c0024.questionnaire_app
 
-class DatabaseHelper {
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+
+class DatabaseHelper (context: Context): SQLiteOpenHelper(context, DATABASE_NAME, null, DATABASE_VERSION){
+    //クラス内のprivate定数を宣言するためにcompanion objectブロックとする。
+    companion object{
+    }
+
+    override fun onCreate(db: SQLiteDatabase){
+    }
+
+    override fun onUpgrade(db: SQLiteDatabase, oldVersion: Int, newVersion: Int){}
 }

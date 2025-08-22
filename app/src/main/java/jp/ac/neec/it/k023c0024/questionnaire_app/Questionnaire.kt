b6038bd0227aa8337740de5037a54b2bf72a701e
PANAKA-4696 <212013796+PANAKA-4696@users.noreply.github.com>
2025-08-22 13:55:34 +0900

    問診票画面のページ遷移の処理の追加

--- app/src/main/java/jp/ac/neec/it/k023c0024/questionnaire_app/Questionnaire.kt
+++ app/src/main/java/jp/ac/neec/it/k023c0024/questionnaire_app/Questionnaire.kt
@@ -1,11 +1,25 @@
 package jp.ac.neec.it.k023c0024.questionnaire_app
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class Questionnaire : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_questionnaire)
+
+        val bt_QuestionnaireNext = findViewById<Button>(R.id.btQuestionnaireNext)
+        bt_QuestionnaireNext.setOnClickListener(ClickQuestionnaireNextListener())
+    }
+
+    private inner class ClickQuestionnaireNextListener : View.OnClickListener {
+        override fun onClick(v: View?) {
+            val ClickQuestionnaireNextListener =
+                Intent(this@Questionnaire, LoginCustomer::class.java)
+            startActivity(ClickQuestionnaireNextListener)
+        }
     }
 }

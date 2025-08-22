a303136080cb5b77bf59286276fb3757926dc4b7
PANAKA-4696 <212013796+PANAKA-4696@users.noreply.github.com>
2025-08-22 14:05:47 +0900

    問診票画面2画面目の作成

---
+++ app/src/main/java/jp/ac/neec/it/k023c0024/questionnaire_app/Questionnaire2.kt
@@ -0,0 +1,14 @@
+package jp.ac.neec.it.k023c0024.questionnaire_app
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class Questionnaire2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_questionnaire2)
+    }
+}

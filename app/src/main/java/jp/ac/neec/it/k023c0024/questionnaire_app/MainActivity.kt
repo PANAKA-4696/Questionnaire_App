40358f397a939cc654538d31ae846fdf6731c37c
PANAKA-4696 <212013796+PANAKA-4696@users.noreply.github.com>
2025-08-27 22:35:39 +0900

    btCheckの有効化

--- app/src/main/java/jp/ac/neec/it/k023c0024/questionnaire_app/MainActivity.kt
+++ app/src/main/java/jp/ac/neec/it/k023c0024/questionnaire_app/MainActivity.kt
@@ -16,9 +16,11 @@ class MainActivity : AppCompatActivity() {
 
         val bt_new = findViewById<Button>(R.id.btNew)
         val bt_member = findViewById<Button>(R.id.btMember)
+        val bt_check = findViewById<Button>(R.id.btCheck)
 
         bt_new.setOnClickListener(ClickNewCustomerListener())
         bt_member.setOnClickListener(ClickLoginCustomerListener())
+        bt_check.setOnClickListener(ClickCheckCustomerListener())
     }
 
     private inner class ClickNewCustomerListener : View.OnClickListener {
@@ -32,6 +34,13 @@ class MainActivity : AppCompatActivity() {
         override fun onClick(v: View?) {
             val ClickLoginCustomerListener = Intent(this@MainActivity, LoginCustomer::class.java)
             startActivity(ClickLoginCustomerListener)
+        }
+    }
+
+    private inner class ClickCheckCustomerListener : View.OnClickListener {
+        override fun onClick(v: View?) {
+            val ClickCheckCustomerListener = Intent(this@MainActivity, CustomerInformationConfirm::class.java)
+            startActivity(ClickCheckCustomerListener)
         }
     }
 }

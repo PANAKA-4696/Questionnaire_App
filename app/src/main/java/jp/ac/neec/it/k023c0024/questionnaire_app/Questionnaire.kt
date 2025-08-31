ca61a6ed3536e724e48b9d364876e84d2309cdc4
PANAKA-4696 <212013796+PANAKA-4696@users.noreply.github.com>
2025-08-31 16:22:58 +0900

    時刻を画面に表示させる処理の追加

--- app/src/main/java/jp/ac/neec/it/k023c0024/questionnaire_app/Questionnaire.kt
+++ app/src/main/java/jp/ac/neec/it/k023c0024/questionnaire_app/Questionnaire.kt
@@ -4,12 +4,30 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.EditText
+import android.widget.RadioGroup
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import java.time.LocalDateTime
+import java.time.LocalTime
+import java.time.format.DateTimeFormatter
 
 class Questionnaire : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_questionnaire)
+
+        val id = intent.getStringExtra("id")
+
+        val tvTime = findViewById<TextView>(R.id.tvQuestionnaireNowDayTime)
+        //現在時刻を取得
+        val now = LocalDateTime.now()
+        //フォーマットを定義
+        val formatter = DateTimeFormatter.ofPattern("yyyy/MM/dd HH時")
+        //フォーマットを適用
+        val formattedDateTime = now.format(formatter)
+
+        tvTime.text = formattedDateTime
 
         val bt_QuestionnaireNext = findViewById<Button>(R.id.btQuestionnaireNext)
         bt_QuestionnaireNext.setOnClickListener(ClickQuestionnaireNextListener())

20a9087ce1e26d331b60e0139fa8c0fcb5200d2b
PANAKA-4696 <212013796+PANAKA-4696@users.noreply.github.com>
2025-08-31 16:31:18 +0900

    Questionnaire2にデータの送信

--- app/src/main/java/jp/ac/neec/it/k023c0024/questionnaire_app/Questionnaire.kt
+++ app/src/main/java/jp/ac/neec/it/k023c0024/questionnaire_app/Questionnaire.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
+import android.widget.RadioButton
 import android.widget.RadioGroup
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
@@ -37,9 +38,28 @@ class Questionnaire : AppCompatActivity() {
         override fun onClick(v: View?) {
             val id = intent.getStringExtra("id")
 
+            val Question1 = findViewById<EditText>(R.id.etQuestionnaireQuestion1).text.toString()
+            val Question2 = findViewById<EditText>(R.id.etQuestionnaireQuestion2).text.toString()
+            val Question3 = findViewById<EditText>(R.id.etQuestionnaireQuestion3).text.toString()
+            val Question4 = findViewById<EditText>(R.id.etQuestionnaireQuestion4).text.toString()
+            val Question5 = findViewById<EditText>(R.id.etQuestionnaireQuestion5).text.toString()
+
+            val rgQuestion6 = findViewById<RadioGroup>(R.id.rgQuestionnaireQuestion6)
+            val question6Id = rgQuestion6.checkedRadioButtonId
+            val Question6 = findViewById<RadioButton>(question6Id).text.toString()
+
+            val etComment = findViewById<EditText>(R.id.etQuestionnaireComment).text.toString()
+
             val ClickQuestionnaireNextListener = Intent(this@Questionnaire, Questionnaire2::class.java)
 
             ClickQuestionnaireNextListener.putExtra("id", id)
+            ClickQuestionnaireNextListener.putExtra("Question1", Question1)
+            ClickQuestionnaireNextListener.putExtra("Question2", Question2)
+            ClickQuestionnaireNextListener.putExtra("Question3", Question3)
+            ClickQuestionnaireNextListener.putExtra("Question4", Question4)
+            ClickQuestionnaireNextListener.putExtra("Question5", Question5)
+            ClickQuestionnaireNextListener.putExtra("Question6", Question6)
+            ClickQuestionnaireNextListener.putExtra("etComment", etComment)
 
             startActivity(ClickQuestionnaireNextListener)
         }

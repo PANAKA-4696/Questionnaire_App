e11d04f93a9ceb877d4ce059f95ddfb1bd4fb053
PANAKA-4696 <212013796+PANAKA-4696@users.noreply.github.com>
2025-09-02 22:45:04 +0900

    Questionnaireの画面のTextViewにDBから持ってきたデータを割り当て

--- app/src/main/java/jp/ac/neec/it/k023c0024/questionnaire_app/Questionnaire.kt
+++ app/src/main/java/jp/ac/neec/it/k023c0024/questionnaire_app/Questionnaire.kt
@@ -2,6 +2,7 @@ package jp.ac.neec.it.k023c0024.questionnaire_app
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
@@ -14,6 +15,8 @@ import java.time.LocalTime
 import java.time.format.DateTimeFormatter
 
 class Questionnaire : AppCompatActivity() {
+    private val _helper = DatabaseHelper(this@Questionnaire)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_questionnaire)
@@ -29,6 +32,61 @@ class Questionnaire : AppCompatActivity() {
         val formattedDateTime = now.format(formatter)
 
         tvTime.text = formattedDateTime
+
+        //データベースヘルパーオブジェクトからデータベース接続オブジェクトを取得
+        val  db = _helper.readableDatabase
+        //主キーによる検索SQL文字列の用意
+        val sql = "SELECT * FROM customer WHERE _id = ${id}"
+        //SQLの実行
+        val cursor = db.rawQuery(sql, null)
+        //データベースから取得した値を格納する変数の用意。データが無かった時のための初期値も用意
+        var kana = ""
+        var name = ""
+        var old = 0
+        var role = ""
+
+        try{//tryブロックで例外発生に備える
+            //カーソルを最初の行に移動させる
+            if(cursor.moveToFirst()) {
+                //カラムのインデックス値を取得(カラム名が正しいかを確認)
+                val idxKana = cursor.getColumnIndex("kana")
+                val idxName = cursor.getColumnIndex("name")
+                val idxOld = cursor.getColumnIndex("old")
+                val idxRole = cursor.getColumnIndex("role")
+
+                //カラムが存在するか確認してからデータを取得(より安全)
+                if (idxKana >= 0) {
+                    kana = cursor.getString(idxKana)
+                }
+                if (idxName >= 0) {
+                    name = cursor.getString(idxName)
+                }
+                if (idxOld >= 0) {
+                    old = cursor.getInt(idxOld)
+                }
+                if (idxRole >= 0) {
+                    role = cursor.getString(idxRole)
+                }
+            }else{
+                //データが見つからなかった場合の処理
+                Log.w("tag", "データが見つかりませんでした")
+            }
+        }catch (e: Exception){
+            Log.e("tag", "例外が発生しました", e)
+            //エラー発生時の処理
+        }finally {
+            //必ずカーソルをクローズする
+            cursor.close()
+        }
+        //取得した値を画面に表示
+        val tvKana = findViewById<TextView>(R.id.tvQuestionnairePutKana)
+        tvKana.text = kana
+        val tvName = findViewById<TextView>(R.id.tvQuestionnairePutName)
+        tvName.text = name
+        val tvOld = findViewById<TextView>(R.id.tvQuestionnairePutOld)
+        tvOld.text = old.toString()
+        val tvRole = findViewById<TextView>(R.id.tvQuestionnairePutRole)
+        tvRole.text = role
 
         val bt_QuestionnaireNext = findViewById<Button>(R.id.btQuestionnaireNext)
         bt_QuestionnaireNext.setOnClickListener(ClickQuestionnaireNextListener())

8fdb3c00297ae1b4f332e70f029bd1149266a003
PANAKA-4696 <212013796+PANAKA-4696@users.noreply.github.com>
2025-09-08 12:19:28 +0900

    ボタン処理の基礎作成

--- app/src/main/java/jp/ac/neec/it/k023c0024/questionnaire_app/MainActivity.kt
+++ app/src/main/java/jp/ac/neec/it/k023c0024/questionnaire_app/MainActivity.kt
@@ -4,12 +4,11 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    val _helper = DatabaseHelper(this@MainActivity)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -17,10 +16,14 @@ class MainActivity : AppCompatActivity() {
         val bt_new = findViewById<Button>(R.id.btNew)
         val bt_member = findViewById<Button>(R.id.btMember)
         val bt_check = findViewById<Button>(R.id.btCheck)
+        val bt_read_google_drive = findViewById<Button>(R.id.btReadGoogleDrive)
+        val bt_write_google_drive = findViewById<Button>(R.id.btWriteGoogleDrive)
 
         bt_new.setOnClickListener(ClickNewCustomerListener())
         bt_member.setOnClickListener(ClickLoginCustomerListener())
         bt_check.setOnClickListener(ClickCheckCustomerListener())
+        bt_read_google_drive.setOnClickListener(ClickReadGoogleDriveListener())
+        bt_write_google_drive.setOnClickListener(ClickWriteGoogleDriveListener())
     }
 
     private inner class ClickNewCustomerListener : View.OnClickListener {
@@ -41,6 +44,18 @@ class MainActivity : AppCompatActivity() {
         override fun onClick(v: View?) {
             val ClickCheckCustomerListener = Intent(this@MainActivity, InfomationCheckLogin::class.java)
             startActivity(ClickCheckCustomerListener)
+        }
+    }
+
+    private inner class ClickReadGoogleDriveListener : View.OnClickListener {
+        override fun onClick(v: View?) {
+
+        }
+    }
+
+    private inner class ClickWriteGoogleDriveListener : View.OnClickListener {
+        override fun onClick(v: View?) {
+
         }
     }
 }

4f05b53c3027c7fb119c365963a117069893857c
PANAKA-4696 <212013796+PANAKA-4696@users.noreply.github.com>
2025-09-08 13:44:56 +0900

    API使用のための変更

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -31,11 +31,17 @@ android {
         targetCompatibility = JavaVersion.VERSION_11
     }
     kotlinOptions {
-        jvmTarget = "11"
+        jvmTarget = "17"
     }
 }
 
 dependencies {
+    // この一行があるか確認
+    implementation("com.google.android.gms:play-services-auth:21.2.0") // 括弧を追加
+
+    // Drive APIも後で使うので必要
+    implementation("com.google.api-client:google-api-client-android:2.4.0") // 括弧を追加
+    implementation("com.google.apis:google-api-services-drive:v3-rev20240725-2.0.0") // 括弧を追加
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
@@ -45,4 +51,7 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+    // Coroutinesを追加
+    implementation(libs.kotlinx.coroutines.android)
 }

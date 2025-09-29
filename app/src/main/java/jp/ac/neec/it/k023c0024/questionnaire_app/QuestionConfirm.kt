57b3894c46824f2f867f48371b48d5ef7cc44f44
PANAKA-4696 <212013796+PANAKA-4696@users.noreply.github.com>
2025-09-29 23:07:21 +0900

    QuestionConfirmの立ち上げ

---
+++ app/src/main/java/jp/ac/neec/it/k023c0024/questionnaire_app/QuestionConfirm.kt
@@ -0,0 +1,20 @@
+package jp.ac.neec.it.k023c0024.questionnaire_app
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class QuestionConfirm : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_question_confirm)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}

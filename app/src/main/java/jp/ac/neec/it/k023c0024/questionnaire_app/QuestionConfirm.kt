0e5bb16f8899f5cb16007ba03d3b1e06b0ba2038
PANAKA-4696 <212013796+PANAKA-4696@users.noreply.github.com>
2025-10-02 23:31:05 +0900

    QuestionConfirmのひな形完成

--- app/src/main/java/jp/ac/neec/it/k023c0024/questionnaire_app/QuestionConfirm.kt
+++ app/src/main/java/jp/ac/neec/it/k023c0024/questionnaire_app/QuestionConfirm.kt
@@ -7,6 +7,8 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class QuestionConfirm : AppCompatActivity() {
+    val _databaseHelper = DatabaseHelper(this@QuestionConfirm)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_question_confirm)

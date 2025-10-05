19d1005f7e36f1e1577275c2d94b3f63a8e4a1b6
PANAKA-4696 <212013796+PANAKA-4696@users.noreply.github.com>
2025-10-05 23:53:44 +0900

    ReQuestionnaireの処理の作成

--- app/src/main/java/jp/ac/neec/it/k023c0024/questionnaire_app/ReQuestionnaire.kt
+++ app/src/main/java/jp/ac/neec/it/k023c0024/questionnaire_app/ReQuestionnaire.kt
@@ -1,14 +1,210 @@
 package jp.ac.neec.it.k023c0024.questionnaire_app
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.view.MenuItem
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.RadioButton
+import android.widget.RadioGroup
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
 
 class ReQuestionnaire : AppCompatActivity() {
+    val _helper = DatabaseHelper(this@ReQuestionnaire)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_re_questionnaire)
+
+        val id = intent.getStringExtra("id")
+        val date = intent.getStringExtra("date")
+
+        val tvTime = findViewById<TextView>(R.id.tvReQuestionnaireNowDayTime)
+        tvTime.text = date
+
+        //データベースヘルパーオブジェクトからデータベース接続オブジェクトを取得
+        var db = _helper.readableDatabase
+        //主キーによる検索SQL文字列の用意
+        var sql = "SELECT * FROM customer WHERE _id = ?"
+        //SQLの実行
+        var cursor = db.rawQuery(sql, arrayOf(id))
+        //データベースから取得した値を格納する変数の用意。データが無かった時のための初期値も用意
+        var kana = ""
+        var name = ""
+        var old = 0
+        var role = ""
+
+        try {//tryブロックで例外発生に備える
+            //カーソルを最初の行に移動させる
+            if (cursor.moveToFirst()) {
+                //カラムのインデックス値を取得(カラム名が正しいかを確認)
+                val idxKana = cursor.getColumnIndex("kana")
+                val idxName = cursor.getColumnIndex("name")
+                val idxOld = cursor.getColumnIndex("old")
+                val idxRole = cursor.getColumnIndex("role")
+
+                //カラムが存在するか確認してからデータを取得(より安全)
+                if (idxKana >= 0) {
+                    kana = cursor.getString(idxKana)
+                }
+                if (idxName >= 0) {
+                    name = cursor.getString(idxName)
+                }
+                if (idxOld >= 0) {
+                    old = cursor.getInt(idxOld)
+                }
+                if (idxRole >= 0) {
+                    role = cursor.getString(idxRole)
+                }
+            } else {
+                //データが見つからなかった場合の処理
+                Log.w("tag", "データが見つかりませんでした")
+            }
+        } catch (e: Exception) {
+            Log.e("tag", "例外が発生しました", e)
+            //エラー発生時の処理
+        } finally {
+            //必ずカーソルをクローズする
+            cursor.close()
+        }
+        //取得した値を画面に表示
+        val tvKana = findViewById<TextView>(R.id.tvReQuestionnairePutKana)
+        tvKana.text = kana
+        val tvName = findViewById<TextView>(R.id.tvReQuestionnairePutName)
+        tvName.text = name
+        val tvOld = findViewById<TextView>(R.id.tvReQuestionnairePutOld)
+        tvOld.text = old.toString()
+        val tvRole = findViewById<TextView>(R.id.tvReQuestionnairePutRole)
+        tvRole.text = role
+
+        db = _helper.readableDatabase
+        sql = "SELECT * FROM questionnaire WHERE _id = ? AND time = ?"
+
+        cursor = db.rawQuery(sql, arrayOf(id,date))
+
+        var question1 = ""
+        var question2 = ""
+        var question3 = ""
+        var question4 = ""
+        var question5 = ""
+        var question6 = ""
+        var questionComment = ""
+
+        try{
+            if(cursor.moveToFirst()){
+                val idxQuestion1 = cursor.getColumnIndex("Question1")
+                val idxQuestion2 = cursor.getColumnIndex("Question2")
+                val idxQuestion3 = cursor.getColumnIndex("Question3")
+                val idxQuestion4 = cursor.getColumnIndex("Question4")
+                val idxQuestion5 = cursor.getColumnIndex("Question5")
+                val idxQuestion6 = cursor.getColumnIndex("Question6")
+                val idxQuestionComment = cursor.getColumnIndex("etComment")
+
+                if(idxQuestion1 >= 0){
+                    question1 = cursor.getString(idxQuestion1)
+                }
+                if(idxQuestion2 >= 0){
+                    question2 = cursor.getString(idxQuestion2)
+                }
+                if(idxQuestion3 >= 0){
+                    question3 = cursor.getString(idxQuestion3)
+                }
+                if(idxQuestion4 >= 0){
+                    question4 = cursor.getString(idxQuestion4)
+                }
+                if(idxQuestion5 >= 0){
+                    question5 = cursor.getString(idxQuestion5)
+                }
+                if(idxQuestion6 >= 0){
+                    question6 = cursor.getString(idxQuestion6)
+                }
+                if(idxQuestionComment >= 0){
+                    questionComment = cursor.getString(idxQuestionComment)
+                }
+            }else{
+                Log.w("tag", "データが見つかりませんでした。")
+            }
+        }catch (e:Exception){
+            Log.e("tag", "例外が発生しました。", e)
+        }finally {
+            cursor.close()
+        }
+
+        val puQuestion1 = findViewById<EditText>(R.id.etQuestionnaireQuestion1)
+        puQuestion1.text = question1
+        val puQuestion2 = findViewById<EditText>(R.id.etQuestionnaireQuestion2)
+        puQuestion2.text = question2
+        val puQuestion3 = findViewById<EditText>(R.id.etQuestionnaireQuestion3)
+        puQuestion3.text = question3
+        val puQuestion4 = findViewById<EditText>(R.id.etQuestionnaireQuestion4)
+        puQuestion4.text = question4
+        val puQuestion5 = findViewById<EditText>(R.id.etQuestionnaireQuestion5)
+        puQuestion5.text = question5
+        val puQuestion6 = findViewById<EditText>(R.id.etQuestionnaireComment)
+        puQuestion6.text = question6
+        val puQuestionComment = findViewById<EditText>(R.id.etReQuestionnaireComment)
+        puQuestionComment.text = questionComment
+
+        val bt_QuestionnaireNext = findViewById<Button>(R.id.btQuestionnaireNext)
+        bt_QuestionnaireNext.setOnClickListener(ClickQuestionnaireNextListener())
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    private inner class ClickQuestionnaireNextListener : View.OnClickListener {
+        override fun onClick(v: View?) {
+            val id = intent.getStringExtra("id")
+
+            val Question1 = findViewById<EditText>(R.id.etQuestionnaireQuestion1).text.toString()
+            val Question2 = findViewById<EditText>(R.id.etQuestionnaireQuestion2).text.toString()
+            val Question3 = findViewById<EditText>(R.id.etQuestionnaireQuestion3).text.toString()
+            val Question4 = findViewById<EditText>(R.id.etQuestionnaireQuestion4).text.toString()
+            val Question5 = findViewById<EditText>(R.id.etQuestionnaireQuestion5).text.toString()
+
+            val rgQuestion6 = findViewById<RadioGroup>(R.id.rgQuestionnaireQuestion6)
+            val question6Id = rgQuestion6.checkedRadioButtonId
+            val Question6 = findViewById<RadioButton>(question6Id).text.toString()
+
+            val etComment = findViewById<EditText>(R.id.etQuestionnaireComment).text.toString()
+
+            val time = findViewById<TextView>(R.id.tvReQuestionnaireNowDayTime).text.toString()
+
+
+            val ClickQuestionnaireNextListener =
+                Intent(this@ReQuestionnaire, Questionnaire2::class.java)
+
+            ClickQuestionnaireNextListener.putExtra("time", time)
+            ClickQuestionnaireNextListener.putExtra("id", id)
+            ClickQuestionnaireNextListener.putExtra("Question1", Question1)
+            ClickQuestionnaireNextListener.putExtra("Question2", Question2)
+            ClickQuestionnaireNextListener.putExtra("Question3", Question3)
+            ClickQuestionnaireNextListener.putExtra("Question4", Question4)
+            ClickQuestionnaireNextListener.putExtra("Question5", Question5)
+            ClickQuestionnaireNextListener.putExtra("Question6", Question6)
+            ClickQuestionnaireNextListener.putExtra("etComment", etComment)
+
+            startActivity(ClickQuestionnaireNextListener)
+        }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        //戻り値用の変数を初期値trueで用意
+        var returnVal = true
+        //選択されたメニューが「戻る」の場合、アクティビティを終了
+        if (item.itemId == android.R.id.home) {
+            finish()
+        } else {
+            //それ以外の場合、戻り値用の変数をfalseに設定
+            returnVal = super.onOptionsItemSelected(item)
+        }
+        return returnVal
     }
 }

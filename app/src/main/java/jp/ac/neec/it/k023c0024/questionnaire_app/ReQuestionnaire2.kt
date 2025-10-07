d095c33cde8c94e1824e0e93bb3a82f0f3694c11
PANAKA-4696 <212013796+PANAKA-4696@users.noreply.github.com>
2025-10-07 21:13:40 +0900

    ReQuestionnaireのコピーによる定義名の重なりをなくすために修正

--- app/src/main/java/jp/ac/neec/it/k023c0024/questionnaire_app/ReQuestionnaire2.kt
+++ app/src/main/java/jp/ac/neec/it/k023c0024/questionnaire_app/ReQuestionnaire2.kt
@@ -1,14 +1,94 @@
 package jp.ac.neec.it.k023c0024.questionnaire_app
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.MenuItem
+import android.view.View
+import android.widget.Button
+import android.widget.CheckBox
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class ReQuestionnaire2 : AppCompatActivity() {
+    private val _helper = DatabaseHelper(this@ReQuestionnaire2)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_re_questionnaire2)
+
+        val bt_re_questionnaire_confirm = findViewById<Button>(R.id.btReQuestionnaireConfirm)
+        bt_re_questionnaire_confirm.setOnClickListener(ReQuestionnaireConfirmListener())
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    private inner class ReQuestionnaireConfirmListener : View.OnClickListener{
+        override fun onClick(v: View?) {
+            val cb_requestionnaire2 = findViewById<CheckBox>(R.id.cbReQuestionnaire2)
+            if (cb_requestionnaire2.isChecked) {
+
+                val db = _helper.writableDatabase
+                val time = intent.getStringExtra("time")
+                val id = intent.getStringExtra("id")
+                val Question1 = intent.getStringExtra("Question1")
+                val Question2 = intent.getStringExtra("Question2")
+                val Question3 = intent.getStringExtra("Question3")
+                val Question4 = intent.getStringExtra("Question4")
+                val Question5 = intent.getStringExtra("Question5")
+                val Question6 = intent.getStringExtra("Question6")
+                val etComment = intent.getStringExtra("etComment")
+
+                val idAsLong = id?.toLongOrNull()
+
+                try {
+                    if (time != null && idAsLong != null && Question1 != null && Question2 != null && Question3 != null && Question4 != null && Question5 != null && Question6 != null && etComment != null) {
+                        val sqlInsert = "INSERT INTO questionnaire (time, _id, Question1, Question2, Question3, Question4, Question5, Question6, etComment) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)"
+                        val stmt = db.compileStatement(sqlInsert)
+
+                        stmt.bindString(1, time)
+                        stmt.bindLong(2, idAsLong)
+                        stmt.bindString(3, Question1)
+                        stmt.bindString(4, Question2)
+                        stmt.bindString(5, Question3)
+                        stmt.bindString(6, Question4)
+                        stmt.bindString(7, Question5)
+                        stmt.bindString(8, Question6)
+                        stmt.bindString(9, etComment)
+
+                        stmt.executeInsert()
+                        Toast.makeText(this@ReQuestionnaire2, "回答を提出しました", Toast.LENGTH_LONG).show()
+
+                        //MainActivityに遷移
+                        val intent = Intent(this@ReQuestionnaire2, MainActivity::class.java)
+                        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_SINGLE_TOP)
+                        startActivity(intent)
+                        finish()
+
+                    } else {
+                        Toast.makeText(this@ReQuestionnaire2, "回答に失敗しました", Toast.LENGTH_LONG).show()
+                    }
+                }finally {
+                    db.close()
+                }
+            }else{
+                Toast.makeText(this@ReQuestionnaire2, "同意が必要です", Toast.LENGTH_LONG).show()
+            }
+        }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean{
+        //戻り値用の変数を初期値trueで用意
+        var returnVal = true
+        //選択されたメニューが「戻る」の場合、アクティビティを終了
+        if(item.itemId == android.R.id.home){
+            finish()
+        }else{
+            //それ以外の場合、戻り値用の変数をfalseに設定
+            returnVal = super.onOptionsItemSelected(item)
+        }
+        return returnVal
     }
 }

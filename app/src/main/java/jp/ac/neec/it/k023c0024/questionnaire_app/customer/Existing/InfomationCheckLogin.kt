1ce3af7b756391625ceab58b0bd0964b1cce5511
PANAKA-4696 <212013796+PANAKA-4696@users.noreply.github.com>
2025-10-29 21:54:00 +0900

    createCustomerListの削除

--- app/src/main/java/jp/ac/neec/it/k023c0024/questionnaire_app/customer/Existing/InfomationCheckLogin.kt
+++ app/src/main/java/jp/ac/neec/it/k023c0024/questionnaire_app/customer/Existing/InfomationCheckLogin.kt
@@ -52,38 +52,6 @@ class InfomationCheckLogin : AppCompatActivity() {
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
     }
 
-    private fun createCustomerList(): MutableList<MutableMap<String, String>> {
-        //既存のリストをクリアする
-        _customerList.clear()
-
-        //データベースヘルパーオブジェクトからデータベース接続オブジェクトを取得
-        val db = _helper.readableDatabase
-        //主キーにより検索SQL文字列の用意
-        val sql = "SELECT * FROM customer"
-        //SQLの実行
-        val cursor = db.rawQuery(sql, null)
-        //データベースから取得した値を格納する変数の用意。データが無かった時のための初期値も用意
-        var name = ""
-        var id = ""
-
-        //SQL実行の戻り値で圧カーソルオブジェクトをループさせてデータベース内のデータを取得
-        while(cursor.moveToNext()){
-            //カラムのインデックス値を取得
-            val idxid = cursor.getColumnIndex("_id")
-            val idxname = cursor.getColumnIndex("name")
-            //カラムのインデックス値を元に実際のデータを取得
-            id = cursor.getString(idxid)
-            name = cursor.getString(idxname)
-            //取得したデータをリストに追加
-            _customerList.add(mutableMapOf("id" to id, "name" to name))
-        }
-
-        //カーソルを閉じる
-        cursor.close()
-
-        return _customerList
-    }
-
     override fun onResume() {
         super.onResume()
 
